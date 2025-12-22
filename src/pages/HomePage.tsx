829ef3deee742f9ab6447f67ac7154068371a31e
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-22 10:18:15 -0800

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/Hero"
 import HeroParallaxDemo from "@/components/hero-parallax-demo"
-import FloatingParticles from "@/components/FloatingParticles"
 
 export default function HomePage() {
   return (
     <>
-      <FloatingParticles />
       <Hero />
       <HeroParallaxDemo />
     </>
