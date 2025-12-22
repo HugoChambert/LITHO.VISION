5380cd3eb07be6f0f67b778cbc1303849fde03b8
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-22 07:03:36 -0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import HeroParallaxDemo from "@/components/hero-parallax-demo"
+
 function App() {
   return (
     <div className="bg-black min-h-screen">
+      <HeroParallaxDemo />
     </div>
   )
 }
