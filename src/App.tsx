62ee275135fc2829c55eb2f6d90ee562596dcf51
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-22 07:01:34 -0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import SidebarDemo from "@/components/sidebar-demo"
-
 function App() {
   return (
-    <div className="bg-black min-h-screen flex items-center justify-center p-4">
-      <SidebarDemo />
+    <div className="bg-black min-h-screen">
     </div>
   )
 }
