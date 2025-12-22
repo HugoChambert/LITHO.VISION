616cad53685312298940ea506dcf477014c96765
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-22 09:39:44 -0800

    Updated index.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from "@/components/Navbar"
 import HomePage from "@/pages/HomePage"
 import HowItWorksPage from "@/pages/HowItWorksPage"
+import CustomCursor from "@/components/CustomCursor"
 
 function App() {
   return (
     <Router>
       <div className="bg-black">
+        <CustomCursor />
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
