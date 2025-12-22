e0268faad9e5c57eb303709c7da43c5974be64ac
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-22 07:38:44 -0800

    Updated package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import HeroParallaxDemo from "@/components/hero-parallax-demo"
 import Navbar from "@/components/Navbar"
-import SplineLanding from "@/components/SplineLanding"
 
 function App() {
   return (
     <div className="bg-black min-h-screen">
       <Navbar />
-      <SplineLanding />
       <HeroParallaxDemo />
     </div>
   )
