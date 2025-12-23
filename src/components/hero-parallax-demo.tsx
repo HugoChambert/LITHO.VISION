e96d4e5e73c0f9e85bd28226d7a1293a6c2c60c6
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-23 13:57:33 -0500

    Update hero-parallax-demo.tsx

--- src/components/hero-parallax-demo.tsx
+++ src/components/hero-parallax-demo.tsx
@@ -9,54 +9,66 @@ export const products = [
   // Row 1 (scrolls right to left)
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t1.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t2.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t3.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t4.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t5.png",
   },
 
   // Row 2 (scrolls left to right)
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t6.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t7.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t8.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t9.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t10.png",
   },
 
   // Row 3 (scrolls right to left)
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t11.png",
   },
   {
     title: "Testimonial",
+    link: "javascript:void(0)",
     thumbnail: "/assets/img/t12.png",
   },
 ];
