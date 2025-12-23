de3eac7237ef45c7fdca6e79a1357830e6bd8778
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-23 11:58:49 -0800

    Updated hero-parallax.tsx

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -62,7 +62,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[300vh] py-40 overflow-hidden antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[180vh] py-20 overflow-hidden antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
       style={{
         background: '#000000',
       }}
@@ -120,7 +120,7 @@ export const HeroParallax = ({
 
 export const Header = () => {
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
+    <div className="max-w-7xl relative mx-auto py-10 md:py-20 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold text-white drop-shadow-lg">
         Testimonials From Past Clients
       </h1>
